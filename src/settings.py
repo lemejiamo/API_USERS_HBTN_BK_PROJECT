fb885ab1cf93c4ef22d56837e432dfdd1cc6ea9d
Jaime Aricapa <ja95aricapa@gmail.com>
2022-10-17 10:07:58 -0500

    update env

--- src/settings.py
+++ src/settings.py
@@ -8,22 +8,7 @@ load_dotenv()
 class Settings:
     PORT = int(os.getenv("PORT", 8000))
 
-    ROOT_PATH = "/login"
-
-    BASE_URL = os.getenv("BASE_URL")
-    CRUD_RAW_URL = os.getenv("USERS_RAW_URL")
-    PRODUCT_RAW_URL = os.getenv("VACANCY_RAW_URL")
-
-    CRUD_ROOT_PATH = "/crud"
-    PRODUCT_ROOT_PATH = "/product"
-
     CRUD_API_URL = os.getenv("CRUD_API_URL")
-    PRODUCT_API_URL = os.getenv("JOB_API_URL")
-
-    if not CRUD_API_URL:
-        CRUD_API_URL = f"{BASE_URL}{CRUD_ROOT_PATH}"
-    if not PRODUCT_API_URL:
-        JOB_API_URL = f"{BASE_URL}{PRODUCT_ROOT_PATH}"
 
     ALLOW_ORIGINS = os.getenv("ALLOW_ORIGINS")
     ALLOW_CREDENTIALS = os.getenv("ALLOW_CREDENTIALS")
@@ -32,4 +17,3 @@ class Settings:
 
     SECRET_TOKEN = os.getenv("SECRET_TOKEN")
     TOKEN_ALGORITHM = os.getenv("TOKEN_ALGORITHM")
-    LOGIN = False if os.getenv("LOGIN") == "False" else True
