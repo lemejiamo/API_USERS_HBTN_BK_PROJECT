0c016a70f7c2856cb928bcf78c9db3988936e4bb
Jaime Aricapa <ja95aricapa@gmail.com>
2022-10-20 11:13:40 -0500

    update env

--- src/main.py
+++ src/main.py
@@ -24,4 +24,4 @@ app.add_middleware(
 app.include_router(user.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=settings.PORT, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=settings.PORT)
